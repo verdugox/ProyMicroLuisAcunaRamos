4858e5c68ac5fe15b3526a0836b1c181e9ecacb7
Luis Acuña <luisandresverdugox@gmail.com>
2022-10-25 22:20:03 -0500

    Subiendo proyecto de Spring Initializr

---
+++ ProyectoLuisAcuna/src/main/java/com/LAAR/AFP/Bootcamp/ProyectoLuisAcunaApplication.java
@@ -0,0 +1,13 @@
+package com.LAAR.AFP.Bootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoLuisAcunaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoLuisAcunaApplication.class, args);
+	}
+
+}
