6377ae7d6cc6fe98969dfa0cc635150c05e82372
Luis Acuña <luisandresverdugox@gmail.com>
2022-10-25 23:22:41 -0500

    Subiendo cambios de librerías y application properties

--- ProyectoLuisAcuna/src/main/java/com/LAAR/AFP/Bootcamp/ProyectoLuisAcunaApplication.java
+++ ProyectoLuisAcuna/src/main/java/com/LAAR/AFP/Bootcamp/ProyectoLuisAcunaApplication.java
@@ -1,12 +1,25 @@
 package com.LAAR.AFP.Bootcamp;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.impl.StaticLoggerBinder;
+
+import java.lang.invoke.MethodHandles;
+
 
 @SpringBootApplication
 public class ProyectoLuisAcunaApplication {
 
-	public static void main(String[] args) {
+	//SLF4J's Se logea e instancia la clase
+	private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+	//this is what SLF4J uses to bind to a specific loggin umple
+	public static void main(String[] args)
+	{
 		SpringApplication.run(ProyectoLuisAcunaApplication.class, args);
 	}
 
