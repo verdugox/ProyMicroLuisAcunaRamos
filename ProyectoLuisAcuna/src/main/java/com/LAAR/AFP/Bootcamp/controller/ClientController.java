2c049d6b9a2cdfa340bdaf016c63f1594bda774a
Luis Acuña <luisandresverdugox@gmail.com>
2022-11-01 17:53:56 -0500

    Agregando OpenApiv3 y Swagger Ui para documentación

--- ProyectoLuisAcuna/src/main/java/com/LAAR/AFP/Bootcamp/controller/ClientController.java
+++ ProyectoLuisAcuna/src/main/java/com/LAAR/AFP/Bootcamp/controller/ClientController.java
@@ -4,6 +4,11 @@ import com.LAAR.AFP.Bootcamp.entities.Client;
 import com.LAAR.AFP.Bootcamp.service.IClientService;
 import io.reactivex.Flowable;
 import io.reactivex.Maybe;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +24,15 @@ public class ClientController {
     @Autowired
     private IClientService service;
 
+    @Operation(summary = "Listar todas las solicitudes de vinculación de los clientes")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Se listaron todos los clientes",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Client.class)) }),
+            @ApiResponse(responseCode = "400", description = "Parametros invalidos",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "No se encontraron registros",
+                    content = @Content) })
     @GetMapping("/findAll")
     ///public List<Client> findAll() throws Exception{
     //    return service.findAll();
@@ -31,6 +45,15 @@ public class ClientController {
         );
     }
 
+    @Operation(summary = "Listar registro de vinculación del clientes por Id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Se encontro el cliente registrado por id",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Client.class)) }),
+            @ApiResponse(responseCode = "400", description = "Parametros invalidos",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "No se encontraron registros del cliente",
+                    content = @Content) })
     @GetMapping("/findId/{id}")
     //public Maybe<Client> findId(@PathVariable("id") Integer id) throws  Exception{
     //    return service.findId(id);
@@ -42,27 +65,69 @@ public class ClientController {
                 .defaultIfEmpty(ResponseEntity.notFound().build());
     }
 
-
+    @Operation(summary = "Listar todos las vinculaciones de los clientes por su AFP")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Se encontro el cliente registrado asociado a su AFP",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Client.class)) }),
+            @ApiResponse(responseCode = "400", description = "Parametros invalidos",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "No se encontraron registros del cliente asociado a su AFP",
+                    content = @Content) })
     @GetMapping("/ClientForAFP/{AFP}")
     public List<Client> getClientForAFP(@PathVariable("AFP") String AFP) throws  Exception{
         return service.getClientForAFP(AFP);
     }
-
+    @Operation(summary = "Listar registro de vinculación del clientes por DNI")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Se encontro el cliente registrado por DNI",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Client.class)) }),
+            @ApiResponse(responseCode = "400", description = "Parametros invalidos",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "No se encontraron registros del cliente por DNI",
+                    content = @Content) })
     @GetMapping("/ClientForDNI/{DNI}")
     public List<Client> getClientForDNI(@PathVariable("DNI") Integer DNI) throws  Exception{
         return service.getClientForDNI(DNI);
     }
 
+    @Operation(summary = "Creación del registro de vinculación del cliente para el AFP")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Se creo el registro de la vinculación del cliente de forma exitosa",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Client.class)) }),
+            @ApiResponse(responseCode = "400", description = "Cliente no se pudo registrar de forma correcta",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Parametros invalidos al registrar",
+                    content = @Content) })
     @PostMapping("/create")
     public Client create(@RequestBody Client client)throws Exception{
         return service.create(client);
     }
 
+    @Operation(summary = "Actualización del registro de vinculación del cliente para el AFP")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Se actualizo el registro de la vinculación del cliente de forma exitosa",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Client.class)) }),
+            @ApiResponse(responseCode = "400", description = "Cliente no se pudo actuualizar de forma correcta",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Parametros invalidos al actualizar",
+                    content = @Content) })
     @PutMapping("/update/{id}")
     public Client update(@PathVariable("id") Long id, @RequestBody Client client) throws Exception{
         return service.update(client, id);
     }
-
+    @Operation(summary = "Eliminación del registro de vinculación del cliente por ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Se elimino el registro de la vinculación del cliente de forma exitosa",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Client.class)) }),
+            @ApiResponse(responseCode = "400", description = "Cliente no se pudo eliminar de forma correcta",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Parametros invalidos al eliminar",
+                    content = @Content) })
     @DeleteMapping("/delete/{id}")
     public void delete(@PathVariable("id") Long id) throws Exception{
           service.delete(id);
--- ProyectoLuisAcuna/src/main/java/com/LAAR/AFP/Bootcamp/entities/Client.java
+++ ProyectoLuisAcuna/src/main/java/com/LAAR/AFP/Bootcamp/entities/Client.java
@@ -2,7 +2,11 @@ package com.LAAR.AFP.Bootcamp.entities;
 
 import io.reactivex.observers.TestObserver;
 import lombok.*;
+import org.hibernate.validator.constraints.Length;
+
 import javax.persistence.*;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
 import java.io.Serializable;
 
 
@@ -21,18 +25,27 @@ public class Client implements Serializable{
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "idClient")
     private Long idClient;
+    @NotBlank
+    @Size(min = 0, max = 60)
     @Column(name = "firstName", nullable = false, length = 60)
     private String firstName;
+    @NotBlank
+    @Size(min = 0, max = 60)
     @Column(name = "lastName", nullable = false, length = 60)
     private String lastName;
     @Column(nullable = false, length = 8)
     private Integer DNI;
     @Column(name = "phone", nullable = false, length = 9)
     private Integer phone;
+    @NotBlank
+    @Size(min = 0, max = 100)
     @Column(nullable = false , length = 100)
     private String email;
+    @NotBlank
+    @Size(min = 0, max = 50)
     @Column(nullable = false, length = 50)
     private String AFP;
+    @DecimalMax("10000000.00") @DecimalMin("0.0")
     @Column(nullable = false, length = 50)
     private Double amountAvailable;
 
